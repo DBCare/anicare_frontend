a623d4828d6d3311c71cb0a6a3495d609b4fdacb
Yunus Emre Öztürk <ozturk.yemr@gmail.com>
2021-10-31 18:13:06 +0300

    Create connection with firebase realtime database

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:untitled/pages/home.dart';
 import 'package:untitled/pages/barcode_results.dart';
 import 'package:untitled/pages/search_product.dart';
 import 'package:untitled/pages/product_details.dart';
+<<<<<<< Updated upstream
 void main() => runApp(MaterialApp(
   initialRoute: '/home',
   routes: {
@@ -13,3 +14,12 @@ void main() => runApp(MaterialApp(
     '/product' : (context) => ProductDetails()
   }
 ));
+=======
+
+void main() => runApp(MaterialApp(initialRoute: '/home', routes: {
+      //'/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/barcode': (context) => BarcodeResults(),
+      '/product': (context) => ProductDetails()
+    }));
+>>>>>>> Stashed changes
