9ebbe84f6fbc0193667051e9eea2bf1e96d46909
Serdil Anıl Ünlü <58235498+serdilanil@users.noreply.github.com>
2022-01-16 16:12:07 +0300

    Add product and branch list pages

---
+++ lib/pages/product_list.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProductList extends StatefulWidget {
+  const ProductList({Key? key}) : super(key: key);
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+        home:Scaffold(
+            backgroundColor: Colors.white70,
+            body: SingleChildScrollView(
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    Row(children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(icon: Icon(Icons.keyboard_arrow_left,size: 40,), onPressed: () { Navigator.of(context).pop(); },),
+                      ),
+                      Container(
+                        child: Row(
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(85,5,5,5),
+                              child: Text('Brands',style: TextStyle(color: Colors.white, fontWeight: FontWeight.normal,fontSize: 15)),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20.0,vertical: 8),
+                          child:
+                          Container(
+                            width: 370,
+                            height: 40,
+                            child:
+                            TextField(
+                              decoration: InputDecoration(
+                                  hintText: "Vegan eyeshadow palette",
+                                  hintStyle: TextStyle(fontSize: 14,color: Color(0xffBAB9D0)),
+                                  prefixIcon: Icon(Icons.search,color: Color(0xff4754F0)),
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      borderSide: BorderSide.none
+                                  ),
+                                  contentPadding: EdgeInsets.zero,
+                                  filled: true,
+                                  fillColor: Color(0xffFCFCFF)
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('All Brands A-Z',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 25),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(150.0,1.0,4.0,5.0),
+                              child: IconButton(icon: Icon(Icons.arrow_forward,),onPressed: () { Navigator.of(context).pop(); }),
+                            ),
+                          ],
+                        )
+                    ),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            TextButton(
+                              onPressed: () {  },
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    child: Image.asset(
+                                      'assets/images/img.png',
+                                      width: 150.0,
+                                      height: 110.0,
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                  Container(
+                                      color: Colors.white,
+                                      width: 150,
+                                      height: 30,
+                                      child: Center(
+                                        child: Text('100% Pure',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 15),
+                                        ),
+                                      )
+                                  )
+                                ],
+                              ),
+                            ),
+
+                            TextButton(
+                              onPressed: () {  },
+                              child: Column(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    child: Image.asset(
+                                      'assets/images/img_1.png',
+                                      width: 150.0,
+                                      height: 110.0,
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                  Container(
+                                      color: Colors.white,
+                                      width: 150,
+                                      height: 30,
+                                      child: Center(
+                                        child: Text('14e Cosmetics',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 15),
+                                        ),
+                                      )
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          child: Row(
+                            children: [
+                              TextButton(
+                                onPressed: () {  },
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      child: Image.asset(
+                                        'assets/images/img_2.png',
+                                        width: 150.0,
+                                        height: 110.0,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                    Container(
+                                        color: Colors.white,
+                                        width: 150,
+                                        height: 30,
+                                        child: Center(
+                                          child: Text('3INA',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 15),
+                                          ),
+                                        )
+                                    )
+                                  ],
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {  },
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      child: Image.asset(
+                                        'assets/images/img_3.png',
+                                        width: 150.0,
+                                        height: 110.0,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                    Container(
+                                        color: Colors.white,
+                                        width: 150,
+                                        height: 30,
+                                        child: Center(
+                                          child: Text('Acure',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 15),
+                                          ),
+                                        )
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          ),
+                        )
+                        ,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          child: Row(
+                            children: [
+                              TextButton(
+                                onPressed: () {  },
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      child: Image.asset(
+                                        'assets/images/img_4.png',
+                                        width: 150.0,
+                                        height: 110.0,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                    Container(
+                                        color: Colors.white,
+                                        width: 150,
+                                        height: 30,
+                                        child: Center(
+                                          child: Text('Adorn Cosmetics',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 14),
+                                          ),
+                                        )
+                                    )
+                                  ],
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {  },
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12.0),
+                                      child: Image.asset(
+                                        'assets/images/img_5.png',
+                                        width: 150.0,
+                                        height: 110.0,
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                    Container(
+                                        color: Colors.white,
+                                        width: 150,
+                                        height: 30,
+                                        child: Center(
+                                          child: Text('AG Hair',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w800,fontSize: 15),
+                                          ),
+                                        )
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+        )
+    );
+  }
+}
